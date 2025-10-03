f75b48a985aa8f246baec2ae1f5e7b862011555b
AkbarAj09 <210210502010@student.unm.ac.id>
2025-10-03 17:22:39 +0700

    update index

--- index.js
+++ index.js
@@ -38,7 +38,15 @@ const app = express();
 const port = 3000;
 
 // === Middleware ===
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
